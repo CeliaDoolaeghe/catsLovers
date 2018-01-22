d6513f43dd22420289e72a2e7d9cff8c45fc7d49
Claudine <claudine.dedecker@decathlon.com>
2018-01-22 11:13:46 +0100

    finalize recompose version

--- src/components/CatsViewer.container.js
+++ src/components/CatsViewer.container.js
@@ -1,43 +1,48 @@
-import { getCats } from '../reducer';
-import { loadCats } from './actions';
+import {getCats} from '../reducer';
+import {loadCats} from './actions';
 import connect from 'react-redux/es/connect/connect';
-import { compose, lifecycle, withHandlers, withState } from 'recompose';
+import {compose, lifecycle, withHandlers, withProps, withState} from 'recompose';
 import CatsViewer from './CatsViewer';
 
 import CATS from '../data/cats.json';
 
 const mapStateToProps = state => ({
-  cats: getCats(state),
+    cats: getCats(state),
 });
 
 const mapDispatchToProps = dispatch => ({
-  loadAllCats: () => {
-    dispatch(loadCats(CATS));
-  }
+    loadAllCats: () => {
+        dispatch(loadCats(CATS));
+    }
 });
 
 const handlers = {
-  previous: ({ setCatIndex, cats, catIndex }) => () => {
-    setCatIndex(catIndex > 0 ? catIndex - 1 : cats.length - 1)
-  },
-  next: ({ setCatIndex, cats, catIndex }) => () => {
-    setCatIndex((catIndex + 1) % cats.length)
-  }
+    previous: ({setCatIndex, cats, catIndex}) => () => {
+        setCatIndex(catIndex > 0 ? catIndex - 1 : cats.length - 1)
+    },
+    next: ({setCatIndex, cats, catIndex}) => () => {
+        setCatIndex((catIndex + 1) % cats.length)
+    }
 };
 
 const lifeCycle = {
-  componentWillMount() {
-    this.props.loadAllCats();
-  }
+    componentWillMount() {
+        this.props.loadAllCats();
+    }
 };
 
+const loadNewProps = ({cats, catIndex}) => ({
+    cat: cats[catIndex],
+    isCatsLoaded: cats.length !== 0,
+});
+
 const enhance = compose(
-  connect(mapStateToProps, mapDispatchToProps),
-  lifecycle(lifeCycle),
-  withState('catIndex', 'setCatIndex', []),
-  withHandlers(handlers),
+    connect(mapStateToProps, mapDispatchToProps),
+    lifecycle(lifeCycle),
+    withState('catIndex', 'setCatIndex', 0),
+    withHandlers(handlers),
+    withProps(loadNewProps)
 );
 
 export default enhance(CatsViewer);
 
-// TODO withProps => cat = cats[catIndex]
--- src/components/CatsViewer.js
+++ src/components/CatsViewer.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import Card from './Card';
 
-export default ({cats, catIndex, previous, next }) => (
-  <div>
-    {cats.length !== 0 &&
-      <div>
-        <div className='row'>
-          <div className='col m4 offset-m4'>
-            <Card cat={cats[catIndex]}/>
-          </div>
+export default ({isCatsLoaded, cat, previous, next}) => (
+    <div>
+        {isCatsLoaded &&
+        <div>
+            <div className='row'>
+                <div className='col m4 offset-m4'>
+                    <Card cat={cat}/>
+                </div>
+            </div>
+            <a className='waves-effect waves-light btn' onClick={previous}>Previous</a> <a
+            className='waves-effect waves-light btn' onClick={next}>Next</a>
         </div>
-        <span>
-        <a className='waves-effect waves-light btn' onClick={previous}>Previous</a>
-          <a className='waves-effect waves-light btn' onClick={next}>Next</a>
-        </span>
-      </div>
-    }
-  </div>
+        }
+    </div>
 );
